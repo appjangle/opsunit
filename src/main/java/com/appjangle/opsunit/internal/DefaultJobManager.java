ce8ea7efe615d213146443cc8ef6b83b17c7f0b6
mxro <mxro@nowhere.com>
2013-01-06 11:21:48 +1300

    changing job execution so that jobs are not executed immideately but only after their usual interval has expirted

--- src/main/java/com/appjangle/opsunit/internal/DefaultJobManager.java
+++ src/main/java/com/appjangle/opsunit/internal/DefaultJobManager.java
@@ -18,162 +18,162 @@ import com.appjangle.opsunit.JobManager;
 
 public class DefaultJobManager implements JobManager {
 
-	private final List<Job> jobs;
-	private final Concurrency concurrency;
-	private final JobExecutorFactory executorFactory;
-	private final JobContext listener;
-	private final List<JobExecutor> activeExecutors;
-	private final List<JobExecutor> scheduledExecutors;
-	private final List<OneTimer> timers;
+    private final List<Job> jobs;
+    private final Concurrency concurrency;
+    private final JobExecutorFactory executorFactory;
+    private final JobContext listener;
+    private final List<JobExecutor> activeExecutors;
+    private final List<JobExecutor> scheduledExecutors;
+    private final List<OneTimer> timers;
 
-	private volatile boolean started = false;
-	private volatile boolean stopping = false;
-	private final OneExecutor workThread;
+    private volatile boolean started = false;
+    private volatile boolean stopping = false;
+    private final OneExecutor workThread;
 
-	@Override
-	public void start() {
-		if (started) {
-			throw new IllegalStateException(
-					"Cannot start an already started job manager.");
-		}
+    @Override
+    public void start() {
+        if (started) {
+            throw new IllegalStateException(
+                    "Cannot start an already started job manager.");
+        }
 
-		if (stopping) {
-			throw new IllegalStateException(
-					"Cannot start an job manager, which is shutting down.");
-		}
+        if (stopping) {
+            throw new IllegalStateException(
+                    "Cannot start an job manager, which is shutting down.");
+        }
 
-		for (final Job job : jobs) {
+        for (final Job job : jobs) {
 
-			final JobExecutor executor = executorFactory.createExecutor(job,
-					listener);
+            final JobExecutor executor = executorFactory.createExecutor(job,
+                    listener);
 
-			final OneTimer jobTimer = concurrency.newTimer().scheduleRepeating(
-					0, job.getFrequency(), new Runnable() {
+            final OneTimer jobTimer = concurrency.newTimer().scheduleRepeating(
+                    job.getFrequency(), job.getFrequency(), new Runnable() {
 
-						@Override
-						public void run() {
-							if (stopping) {
-								return;
-							}
-							if (activeExecutors.contains(executor)) {
-								return;
-							}
-							if (scheduledExecutors.contains(executor)) {
-								return;
-							}
+                        @Override
+                        public void run() {
+                            if (stopping) {
+                                return;
+                            }
+                            if (activeExecutors.contains(executor)) {
+                                return;
+                            }
+                            if (scheduledExecutors.contains(executor)) {
+                                return;
+                            }
 
-							scheduledExecutors.add(executor);
+                            scheduledExecutors.add(executor);
 
-							if (activeExecutors.size() == 0) {
-								runScheduledExecutors();
-								return;
-							}
+                            if (activeExecutors.size() == 0) {
+                                runScheduledExecutors();
+                                return;
+                            }
 
-						}
-					});
-			this.timers.add(jobTimer);
-		}
+                        }
+                    });
+            this.timers.add(jobTimer);
+        }
 
-		started = true;
-	}
+        started = true;
+    }
 
-	private void runScheduledExecutors() {
-		if (scheduledExecutors.size() > 0) {
-			final JobExecutor newExecutor = scheduledExecutors.get(0);
-			scheduledExecutors.remove(0);
+    private void runScheduledExecutors() {
+        if (scheduledExecutors.size() > 0) {
+            final JobExecutor newExecutor = scheduledExecutors.get(0);
+            scheduledExecutors.remove(0);
 
-			activeExecutors.add(newExecutor);
+            activeExecutors.add(newExecutor);
 
-			newExecutor.run(new JobCallback() {
+            newExecutor.run(new JobCallback() {
 
-				@Override
-				public void onDone() {
-					activeExecutors.remove(newExecutor);
+                @Override
+                public void onDone() {
+                    activeExecutors.remove(newExecutor);
 
-					// don't do anything when component is stopping
-					if (stopping) {
-						return;
-					}
+                    // don't do anything when component is stopping
+                    if (stopping) {
+                        return;
+                    }
 
-					// start in new thread to avoid deep recursions
-					workThread.execute(new Runnable() {
+                    // start in new thread to avoid deep recursions
+                    workThread.execute(new Runnable() {
 
-						@Override
-						public void run() {
-							runScheduledExecutors();
-						}
-					});
+                        @Override
+                        public void run() {
+                            runScheduledExecutors();
+                        }
+                    });
 
-				}
-			});
+                }
+            });
 
-		}
-	}
+        }
+    }
 
-	@Override
-	public void stop(final ShutdownCallback callback) {
-		if (!started) {
-			throw new IllegalStateException(
-					"Cannot stop an already stopped job manager.");
+    @Override
+    public void stop(final ShutdownCallback callback) {
+        if (!started) {
+            throw new IllegalStateException(
+                    "Cannot stop an already stopped job manager.");
 
-		}
+        }
 
-		stopping = true;
+        stopping = true;
 
-		for (final OneTimer timer : timers) {
+        for (final OneTimer timer : timers) {
 
-			timer.stop();
+            timer.stop();
 
-		}
+        }
 
-		timers.clear();
+        timers.clear();
 
-		while (activeExecutors.size() > 0) {
-			// System.out.println(activeExecutors.size());
-			try {
-				Thread.sleep(100);
-			} catch (final InterruptedException e) {
+        while (activeExecutors.size() > 0) {
+            // System.out.println(activeExecutors.size());
+            try {
+                Thread.sleep(100);
+            } catch (final InterruptedException e) {
 
-				callback.onFailure(e);
-				return;
-			}
-			Thread.yield();
-		}
+                callback.onFailure(e);
+                return;
+            }
+            Thread.yield();
+        }
 
-		started = false;
-		stopping = false;
+        started = false;
+        stopping = false;
 
-		workThread.shutdown(new WhenExecutorShutDown() {
+        workThread.shutdown(new WhenExecutorShutDown() {
 
-			@Override
-			public void thenDo() {
-				callback.onShutdownComplete();
-			}
+            @Override
+            public void thenDo() {
+                callback.onShutdownComplete();
+            }
 
-			@Override
-			public void onFailure(final Throwable t) {
-				callback.onFailure(t);
-			}
-		});
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+        });
 
-	}
+    }
 
-	public DefaultJobManager(final List<Job> jobs,
-			final Concurrency concurrency,
-			final JobExecutorFactory executorFactory,
-			final JobContext jobContext) {
-		super();
-		this.jobs = jobs;
-		this.concurrency = concurrency;
-		this.listener = jobContext;
-		this.executorFactory = executorFactory;
-		this.timers = new LinkedList<OneTimer>();
-		this.workThread = concurrency.newExecutor().newSingleThreadExecutor(
-				this);
-		this.activeExecutors = concurrency.newCollection().newThreadSafeList(
-				JobExecutor.class);
-		this.scheduledExecutors = concurrency.newCollection()
-				.newThreadSafeList(JobExecutor.class);
-	}
+    public DefaultJobManager(final List<Job> jobs,
+            final Concurrency concurrency,
+            final JobExecutorFactory executorFactory,
+            final JobContext jobContext) {
+        super();
+        this.jobs = jobs;
+        this.concurrency = concurrency;
+        this.listener = jobContext;
+        this.executorFactory = executorFactory;
+        this.timers = new LinkedList<OneTimer>();
+        this.workThread = concurrency.newExecutor().newSingleThreadExecutor(
+                this);
+        this.activeExecutors = concurrency.newCollection().newThreadSafeList(
+                JobExecutor.class);
+        this.scheduledExecutors = concurrency.newCollection()
+                .newThreadSafeList(JobExecutor.class);
+    }
 
 }
